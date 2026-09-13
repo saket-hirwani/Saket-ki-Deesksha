f7304799a309c0229e3addfd464d744948dab2b6
Saket Kumar <sakethirwani15@gmail.com>
2026-09-14 01:34:37 +0530

    Vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './Saket-ki-Deesksha',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
